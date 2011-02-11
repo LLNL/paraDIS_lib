0b5d3660dc62a66e7ec0057766ebf69d55382e43
Rich Cook <wealthychef@yahoo.com>
2011-02-11 18:09:34 +0000

    added binning capability to stats
    Former-commit-id: 591e9b8b58fbccadd47972e08a0d6b4ad8017490

--- paraDIS/paraDIS_lib/analyzeParaDIS.C
+++ paraDIS/paraDIS_lib/analyzeParaDIS.C
@@ -15,7 +15,8 @@ void usage(void) {
   fprintf(stderr, "options:  \n"); 
   fprintf(stderr, " -debugfiles:  dump out detailed analysis in files\n"); 
   fprintf(stderr, " -v (or -verbose) num:  set verbosity level to num (scale of 0-5, 5 is very verbose, 0 is strangely quiet)\n"); 
-  fprintf(stderr, " -threshold num:  report on number of arms of various types which have length less than the given threshold\n"); 
+  fprintf(stderr, " -numbins num: during report, divide total arms into num bins by length and report on the total length in each bin\n"); 
+fprintf(stderr, " -threshold num:  report on number of arms of various types which have length less than the given threshold\n"); 
   fprintf(stderr, " -version: report version and exit\n");
   return; 
 }
@@ -23,17 +24,18 @@ void usage(void) {
 int main(int argc, char *argv[]) {
 
   try {
-    long debugfiles = 0, verbosity=0, help=false, version=false; 
+    long debugfiles = 0, verbosity=0, numbins=0, help=false, version=false; 
     double threshold=-1.0; 
-    argt args[6] = {
+    argt args[7] = {
       {BOOL_TYPE, "-debugfiles", 1, &debugfiles}, 
       {BOOL_TYPE, "-help", 1, &help}, 
+      {LONG_TYPE, "-numbins", 1, &numbins}, 
       {LONG_TYPE, "-v", 1, &verbosity}, 
       {LONG_TYPE, "-verbose", 1, &verbosity}, 
       {DOUBLE_TYPE, "-threshold", 1, &threshold},
       {BOOL_TYPE, "-version", 1, &version}
     }; 
-    arg_expect_args(args, 6);
+    arg_expect_args(args, 7);
     arg_ignore_bad_args(1); 
     if (!arg_parse_args(&argc, argv)) {
       fprintf(stderr, "****************************************\n"); 
@@ -62,6 +64,8 @@ int main(int argc, char *argv[]) {
       gDataSet->SetVerbosity(verbosity, "analyzeParaDIS-debug.txt"); 
     }
     gDataSet->SetThreshold(threshold);
+
+    gDataSet->SetNumBins(numbins);
 
     gDataSet->SetDataFile(argv[1]); 
     
