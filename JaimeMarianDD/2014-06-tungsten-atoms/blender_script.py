48c8deb2ea952dd3d553a2ae518c0cd41a01710c
Rich Cook <cook47@llnl.gov>
2014-06-25 15:47:02 -0700

    most of the important features are there -- now just need to improve things
    Former-commit-id: 2a9fd95b492d56616bd9d1f845ef55e3eb714c17

--- JaimeMarianDD/2014-06-tungsten-atoms/blender_script.py
+++ JaimeMarianDD/2014-06-tungsten-atoms/blender_script.py
@@ -3,26 +3,39 @@
 Blender python script for creating an atomistic rendering for Jaime
 To use, do this in the blender python window.  The script assumes you have done this.  
 import os
-os.chdir('/Users/cook47/current_projects/paraDIS/JaimeMarianDD/2014-06-MDspheres')
+os.chdir('/Users/cook47/current_projects/paraDIS/JaimeMarianDD/2014-06-tungsten-atoms')
 filename = 'blender_script.py'
 exec(compile(open(filename).read(), filename, 'exec'))
 
-You may modify and reload as often as you wish with this syntax. 
+# To load the data, setup the scene, and draw all the atoms: 
+LoadSetupRender()
+
+Or just
+LoadData()
+CreateScene(data)
+
+LoadAtoms(data)
+
+You may modify and reload as often as you wish with this syntax.
+Remember:  Anything you do in blender shows up as a python command in the info window at the top of the GUI!  Very helpful to know this! 
+
 """
 
 import math, sys, json
- 
+
 #========================================================================
 # assume starting from homefile (with cube, camera and light)
-if bpy.context.scene.render.engine=='BLENDER_RENDER':
- print ('Render is = BLENDER_RENDER')
- bpy.context.scene.render.engine="CYCLES"
- print ('Converted to BLENDER_CYCLES')
-else:
- print ('Render is = CYCLES')
+def SetupContext():
+    if bpy.context.scene.render.engine=='BLENDER_RENDER':
+        print ('Render is = BLENDER_RENDER')
+        bpy.context.scene.render.engine="CYCLES"
+        print ('Converted to BLENDER_CYCLES')
+    else:
+        print ('Render is = CYCLES')
+    bpy.data.scenes["Scene"].cycles.samples = 200
+    bpy.data.scenes["Scene"].cycles.preview_samples = 200
+    return
 
-bpy.data.scenes["Scene"].cycles.samples = 200
-bpy.data.scenes["Scene"].cycles.preview_samples = 200
 #========================================================================
 # select the objects whose name contains anything in keynamelist and deselect everything else.
 def select_names(keynamelist, extend = False ):
@@ -97,8 +110,10 @@ def create_light_plane(name, radius, rotation, location, value=1):
      light.data.materials.append(lightmat)
      lightmat.use_nodes = True
      # lightmat.node_tree.nodes.remove(lightmat.node_tree.nodes["Diffuse BSDF"])
+     # http://www.blender.org/documentation/blender_python_api_2_70_5/bpy.types.html
      emitter = lightmat.node_tree.nodes.new("ShaderNodeEmission")
      emitter.inputs['Strength'].default_value = value
+     emitter.name = '%s_emission'%name
      lightmat.node_tree.links.new(emitter.outputs['Emission'], lightmat.node_tree.nodes['Material Output'].inputs['Surface'])
      return light
 
@@ -142,6 +157,13 @@ def createBoundsPlane(name, rotation, location, scale):
     planemat = bpy.data.materials['%s_material'%name]
     plane.data.materials.append(planemat)
     planemat.use_nodes = True
+    glossnode = planemat.node_tree.nodes.new("ShaderNodeBsdfGlossy")
+    glossnode.name = 'glossnode_%s'%name
+    glossnode.inputs['Roughness'].default_value = 0.35
+    glossnode.inputs['Color'].default_value = [0.2,0.2,0.2,1]
+    # planemat.node_tree.nodes.remove(planemat.node_tree.nodes["Diffuse BSDF"])
+    outnode = planemat.node_tree.nodes['Material Output']
+    planemat.node_tree.links.new(glossnode.outputs['BSDF'], outnode.inputs['Surface'])
     return
 
 #========================================================================
@@ -151,33 +173,78 @@ def createBoundsPlanes(data):
     center =  data["center"]
     diag = data["diagonal"]
     # XY plane:
-    createBoundsPlane("XY Plane", (0, 0, 0), (data["center"][0], data["center"][1], data['bounds'][2][0]), [boundsSize[0]/2.0, boundsSize[1]/2.0, 1] )    
+    createBoundsPlane("XY_Plane", (0, 0, 0), (data["center"][0], data["center"][1], data['bounds'][2][0]), [boundsSize[0]/2.0, boundsSize[1]/2.0, 1] )    
     # YZ plane (rotate Y into Z)
-    createBoundsPlane("YZ Plane", (0, pi/2.0, 0), (bounds[0][0], center[1], center[2]), [boundsSize[2]/2.0, boundsSize[1]/2.0, 1])    
+    createBoundsPlane("YZ_Plane", (0, pi/2.0, 0), (bounds[0][0], center[1], center[2]), [boundsSize[2]/2.0, boundsSize[1]/2.0, 1])    
     # XZ plane (rotate X into Z)
-    createBoundsPlane("XZ Plane", (pi/2.0, 0, 0), (center[0], bounds[1][0], center[2]), [boundsSize[0]/2.0, boundsSize[2]/2.0, 1])    
+    createBoundsPlane("XZ_Plane", (pi/2.0, 0, 0), (center[0], bounds[1][0], center[2]), [boundsSize[0]/2.0, boundsSize[2]/2.0, 1])    
     return
+
+
+#========================================================================
+def MakeColorRamp(mat, linknode, colors):
+    ramp = mat.node_tree.nodes.new("ShaderNodeValToRGB")
+    if len(colors) > 2:
+        frac_inc = 1.0 / (len(colors) - 1)
+        frac = frac_inc
+        for i in range(len(colors)-2):
+            ramp.color_ramp.elements.new(frac)
+            frac = frac + frac_inc
+    for i in range(len(colors)):
+        ramp.color_ramp.elements[i].color = colors[i]        
+    mat.node_tree.links.new(ramp.outputs['Color'], linknode.inputs['Color'])
+    return ramp
 
 #========================================================================
 # Ideally, I would combine all atoms into a single mesh for drawing efficiency, but each atom needs its own color, so...
-def make_atom(atom):
-    bpy.ops.mesh.primitive_uv_sphere_add(location=atom["location"], size = 0.5 )
-    atomobj = bpy.data.objects['Sphere']
-    atomobj.name = "Atom %d"%atom['id']
+def MakeAtom(atom):
+    bpy.ops.mesh.primitive_uv_sphere_add(location=atom["location"], size = 1.2 )
+    obj = bpy.data.objects['Sphere']
+    name = "Atom_%d"%atom['id']
+    obj.name = name
+    bpy.data.materials.new('%s_material'%name)
+    mat = bpy.data.materials['%s_material'%name]
+    obj.data.materials.append(mat)
+    mat.use_nodes = True
+    glossnode = mat.node_tree.nodes.new("ShaderNodeBsdfGlossy")
+    glossnode.name = "gloss_%d"%atom['id']
+    glossnode.inputs['Roughness'].default_value = 0.35
+    glossnode.inputs['Color'].default_value = [1,0,1,1]
+    # mat.node_tree.nodes.remove(mat.node_tree.nodes["Diffuse BSDF"])
+    outnode = mat.node_tree.nodes['Material Output']
+    mat.node_tree.links.new(glossnode.outputs['BSDF'], outnode.inputs['Surface'])    
+    colors = [ [1,0,0,1], [1,1,0,1], [1,0,1,1], [0,1,0,1], [0,1,1,1] ]
+    ramp = MakeColorRamp(mat, glossnode, colors)
+    ramp.name = 'ColorRame_%s'%name
+    ramp.inputs['Fac'].default_value = atom['c_centro']/10.0
     return
 
 #========================================================================
-def LoadAtoms(data):
+def MakeAtoms(data):
     for atom in data["atoms"]:
-        make_atom(atom)
+        MakeAtom(atom)
+    select_names("Atom")
+    bpy.ops.object.shade_smooth()
+
+#========================================================================
+def CreateScene(data):
+    SetupContext()
+    createBoundsPlanes(data)
+    SetupCameraAndFrustrum(data)
+
+#========================================================================
+data = {"nothing":None}
+def LoadData():
+    global data
+    os.chdir("/Users/cook47/current_projects/paraDIS/JaimeMarianDD/2014-06-tungsten-atoms")
+    infile = open("MD_300K_1100MPa_d40_slice[1].json")
+    data = json.load(infile)["data"]
+    return 
 
 
-
-# create scene
-os.chdir("/Users/cook47/current_projects/paraDIS/JaimeMarianDD/2014-06-MDspheres")
-infile = open("MD_300K_1100MPa_d40_slice[1].json")
-data = json.load(infile)["data"]
-createBoundsPlanes(data)
-SetupCameraAndFrustrum(data)
-
+#========================================================================
+def LoadSetupRender():
+    LoadData()
+    CreateScene(data)
+    MakeAtoms(data)
 
