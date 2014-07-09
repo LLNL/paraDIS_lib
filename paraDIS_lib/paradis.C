2c0fa1fc5467104f691281c2004e16223430bea2
Richard Cook <rcook@llnl.gov>
2014-07-09 09:21:57 -0700

    document rotation
    Former-commit-id: 384a8bbbfaea64a76616c2c51ef48fd7cd891ef1

--- paraDIS_lib/paradis.C
+++ paraDIS_lib/paradis.C
@@ -936,6 +936,9 @@ namespace paraDIS {
 
   //===========================================================================
   // 	# Thanks to http://mcngraphics.com/thelab/blender/connect/
+  /* Computes a Y and Z rotation that take the segment from a Z orientation
+	 (the blender cylinder uv mesh default) to its proper orientation.
+  */
   std::string ArmSegment::BlenderRotationString(void) const {
 	vector<float> segdir = SegmentDirection(); 
 	dbprintf(5,str(boost::format("BlenderRotationString: SegmentDirection = <%f, %f, %f>\n") % segdir[0] % segdir[1] % segdir[2]).c_str()); 
