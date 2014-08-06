7f0b836f5b18d5f244c33a2469f6870d6693499f
Richard Cook <rcook@llnl.gov>
2014-08-06 11:49:24 -0700

    more sophisticated versioning, update Changelog, and call this 2.4.0
    Former-commit-id: d6e623319eec3ba7135f623bd2f479fac5b973fc

--- paraDIS_lib/setversion.sh
+++ paraDIS_lib/setversion.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
     
+timestampOnly=false
 function usage() {
-    echo "Usage:  setversion.sh version"
+    echo "Usage:  setversion.sh [--timestamp or $version]"
+	echo "If --timestamp, then only changes the timestamp, leaves version alone" 
 }
 
-if [ "x$1" == x ]; then 
+errexit () {
+	echo "$1"
+	exit ${2:-1}
+}
+
+if [ "$1" == "" ]; then 
     usage
     exit 1
 fi
-
-sed -i -e "s/#define RC_PARADIS_VERSION .*/#define RC_PARADIS_VERSION \"$1\"/g" -e "s/#define RC_PARADIS_VERSION_DATE.*/#define RC_PARADIS_VERSION_DATE \"$(date)\"/g" paradis_version.h
+if [ "$1" == "--timestamp" ]; then 
+	sed -i "s/#define RC_PARADIS_VERSION_DATE.*/#define RC_PARADIS_VERSION_DATE \"$(date)\"/g" paradis_version.h
+elif [ ${1:0:1} == '-' ]; then 
+	usage
+	errexit "Unknown option \"$1\""
+else
+	sed -i -e "s/#define RC_PARADIS_VERSION .*/#define RC_PARADIS_VERSION \"$1\"/g" -e "s/#define RC_PARADIS_VERSION_DATE.*/#define RC_PARADIS_VERSION_DATE \"$(date)\"/g" paradis_version.h
+fi
