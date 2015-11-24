2f7ced27c7efe15a22f2d21070279c1dfde83aed
Richard Cook <rcook@llnl.gov>
2015-11-24 11:29:28 -0800

    auto-export to build farm
    Former-commit-id: 016d31a365b7e50f64177d16b462bbfb81bebdff

---
+++ export-to-buildfarm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+cd $(dirname $0)
+version=$(./setversion.sh -p)
+farm_dir=/g/g0/rcook/current_projects/eris/lclocal/public/paradis-lib/branches/paradis-lib-${version}
+export_tarball=${farm_dir}/paradis-lib-v${version}.tar
+
+errexit() {
+    echo "ERROR: $1"
+    exit ${2:-1}
+}
+
+[ -d ${farm_dir} ] || errexit "$farm_dir does not exist -- please create it"
+
+if [ -f $export_tarball ]; then 
+    echo "$export_tarball already exists.  Overwrite? (y/n)"
+    read answer
+    [ "$answer" == "y" ] ||  errexit "OK.  I'll exit then." 0
+    echo "I will overwrite $export_tarball"
+fi
+
+
+git archive  master --prefix paradis-lib-v${version}/ -o ${farm_dir}/paradis-lib-v${version}.tar  || errexit "Could not create ${farm_dir}/paradis-lib-v${version}.tar"
+gzip ${farm_dir}/paradis-lib-v${version}.tar || errexit "Could not compress ${farm_dir}/paradis-lib-v${version}.tar into ${farm_dir}/paradis-lib-v${version}.tar.gz" 
+rm -f ${farm_dir}/paradis-lib-v${version}.tar
+mv ${farm_dir}/paradis-lib-v${version}.tar.gz ${farm_dir}/paradis-lib-v${version}.tgz
+echo "${farm_dir}/paradis-lib-v${version}.tgz created."  
